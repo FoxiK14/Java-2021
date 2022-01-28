7e5c590265598334ecb3c661b311914099b2e7ce
FoxiK14 <47333594+FoxiK14@users.noreply.github.com>
2022-01-28 12:49:49 +0100

    Create Main.java

---
+++ Lab1/Zad1/G/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+import java.lang.Math.*;
+
+public class Main{
+  	public static void main(String[] args){
+    	Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+    	System.out.println("Podaj liczbe naturalna N, nastepnie wprowadz N liczb naturalnych.");
+    	int n = scan.nextInt();
+    	int wynikSuma = 0
+      int wynikIloczyn = 1;
+    	for(int i=0; i<=n; i++){
+          int a = scan.nextInt();
+      		wynikIloczyn *= a;
+          wynikSuma += a;
+    	}
+  	}
+
+}
